eccac018b7991590668ea96ee574df8e7cb50320
blyncnov <bholuwatife00@gmail.com>
2024-08-31 05:54:27 +0100

    Tiles Tales ==> Tiles components

--- src/components/Tiles.vue
+++ src/components/Tiles.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
 const count = ref(0)
 </script>
 
 <template>
-  <div v-for="index in 100" :key="index" class="box" :class="{
-    selected: selectedBoxes.includes(index - 1),
-    highlight: hoveredBoxes.includes(index - 1),
-  }" @dblclick="handleDoubleClick(index - 1)" @mouseover="handleMouseOver(index - 1)" @click="handleClick"></div>
+  <div>
+    <h2>{{ count }}</h2>
+  </div>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
